70ef66702687e01748d1b2bd942e8dd7879708ae
Sergey Ocheretenko <ocheretenko.s@gmail.com>
2022-05-13 12:09:36 +0300

    feat: update PrefixToPostfix function

--- implementation.go
+++ implementation.go
@@ -1,9 +1,15 @@
 package lab2
 
-import "fmt"
+func Reverse(s string) string {
+    runes := []rune(s)
+    for i, j := 0, len(runes)-1; i < j; i, j = i+1, j-1 {
+        runes[i], runes[j] = runes[j], runes[i]
+    }
+    return string(runes)
+}
 
 // TODO: document this function.
 // PrefixToPostfix converts
 func PrefixToPostfix(input string) (string, error) {
-	return "TODO", fmt.Errorf("TODO")
+	return Reverse(input), nil
 }
