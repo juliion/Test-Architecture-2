cc3735dae4322293aaf45b4e3fe86fce5028afa7
juliion <juliaionova111@gmail.com>
2022-05-14 15:14:06 +0300

    Refactor handler

--- handler.go
+++ handler.go
@@ -2,10 +2,9 @@ package lab2
 
 import (
 	"io"
-	"fmt"
-	"log"
 	"bufio"
 )
+
 // ComputeHandler should be constructed with input io.Reader and output io.Writer.
 // Its Compute() method should read the expression from input and write the computed result to the output.
 type ComputeHandler struct {
@@ -14,15 +13,24 @@ type ComputeHandler struct {
 }
 
 func (ch *ComputeHandler) Compute() error {
-	scanner := bufio.NewScanner(ch.Input)
-	for scanner.Scan() {
-		fmt.Println(scanner.Text())
-	}
 
-	if err := scanner.Err(); err != nil {
-		log.Fatal(err)
-	}
-	return nil
+	scanner := bufio.NewScanner(ch.Input)
+	scanner.Scan()
+	expression := scanner.Text()
+	if scanner.Err() != nil {
+        return scanner.Err()
+    }
+	resultExpression, calcErr := PrefixToPostfix(expression)
+	if calcErr != nil {
+		return calcErr
+    }
+	writer := bufio.NewWriter(ch.Output) 
+	_, writeErr := writer.WriteString(resultExpression)
+	if writeErr != nil {
+		return writeErr
+    }
+	writer.Flush()
+	return nil 
 }
 
 
