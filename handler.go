0ada02ca8caed45f8aa4be7180e5ea0973f45a3b
juliion <juliaionova111@gmail.com>
2022-05-14 13:17:44 +0300

    Add hendler

--- handler.go
+++ handler.go
@@ -1,12 +1,28 @@
 package lab2
 
+import (
+	"io"
+	"fmt"
+	"log"
+	"bufio"
+)
 // ComputeHandler should be constructed with input io.Reader and output io.Writer.
 // Its Compute() method should read the expression from input and write the computed result to the output.
 type ComputeHandler struct {
-	// TODO: Add necessary fields.
+	Input io.Reader
+	Output io.Writer
 }
 
 func (ch *ComputeHandler) Compute() error {
-	// TODO: Implement.
+	scanner := bufio.NewScanner(ch.Input)
+	for scanner.Scan() {
+		fmt.Println(scanner.Text())
+	}
+
+	if err := scanner.Err(); err != nil {
+		log.Fatal(err)
+	}
 	return nil
 }
+
+
